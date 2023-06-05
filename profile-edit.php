dcdd4d2a08e1c58f4e7e36e0a5cbd88cae265ef8
joydozer <wuntujoy@gmail.com>
2023-06-06 00:25:42 +0800

    uwaw

--- profile-edit.php
+++ profile-edit.php
@@ -5,6 +5,9 @@
     if(!isset($_SESSION['email'])) {
         header("Location: login.php");
     }
+
+    $dir = "pfp/";
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,21 +27,24 @@
     <div class="main">
         <div class="chat-box profile-box">
             <div class="profile-box-content">
-                <a href="chatlist.html" style="margin-left: 20px;"><img src="image/Arrow 2.png"></a>
+                <a href="chatlist.php" style="margin-left: 20px;"><img src="image/Arrow 2.png"></a>
                 <div class="profile-box-content-mid" style="row-gap: 10px;">
                     <h1> Edit Profile </h1>
+                    <form action="" method="POST" enctype="multipart/form-data" style="text-align: center;">
                     <div class="edit-pfp">
                         <img height="189" width="189" style="border-radius: 50%;" src="image/pfpPlaceholder.png">
                         <div class="edit-pfp-text">
                             Change Picture
+                            <input name="pfp" type="file">
                         </div>
                     </div>
-                    Full Name<br>
-                    <input id="full-name" type="text" placeholder="Full Name"><br>
-                    Bio<br>
-                    <input id="bio" type="text" placeholder="Biography">
-                    <a href="profile.html"><input id="save-changes" type="submit" style="width: 211px; height: 51px;"
-                            value="Save Changes"></a>
+                    <br>
+                    <p style="text-align: left;">Full Name</p>
+                    <input id="full-name" type="text" placeholder="Full Name"><br> <br>
+                    <p style="text-align: left;">Bio</p>
+                    <input id="bio" type="text" placeholder="Biography"> <br>
+                    <input name="submit" id="save-changes" type="submit" style="width: 211px; height: 51px;" value="Save Changes">
+                    </form>
                 </div>
             </div>
         </div>
